ca6e14a55c7645887b32b7d809b7ec471c7a42ca
vybhavb <vybs2499@gmail.com>
2017-02-04 19:47:38 -0800

    index.js created

---
+++ application.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCG36qDp3oC2H3BpqpPBMuLgrxGlKvpvWI",
+	authDomain: "seizeit-cdb9a.firebaseapp.com",
+	databaseURL: "https://seizeit-cdb9a.firebaseio.com",
+	storageBucket: "seizeit-cdb9a.appspot.com",
+	messagingSenderId: "700994103696"
+};
+firebase.initializeApp(config);
